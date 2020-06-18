031b0ec3385a5414cbdcb2f5bec9702fe4c5fe06
刘一二 <liuguoqiang@haitun.hk>
2020-06-18 09:09:53 +0800

    更新数据库结构

--- src/App/System/Service/Db.php
+++ src/App/System/Service/Db.php
@@ -9,31 +9,41 @@ use Be\Util\Str;
 class Db extends \Be\System\Service
 {
 
-
-    public function getTables($app)
+    /**
+     * 获取指定应用下的相关表
+     *
+     * @param string $app 应用
+     * @param string $dbName 库名
+     * @return array
+     */
+    public function getTables($app, $dbName = 'master')
     {
         $tables = [];
         $prefix = Str::camel2Underline($app) . '_';
-        $db = Be::getDb();
+        $db = Be::getDb($dbName);
         $tableNames = $db->getValues('SHOW TABLES LIKE \'' . $prefix . '%\'');
         if ($tableNames) {
             foreach ($tableNames as $tableName) {
-                $tables[] = Be::newTable($tableName);
+                $tables[] = Be::newTable($tableName, $dbName);
             }
         }
         return $tables;
     }
 
-
-    public function updateTableProperty($tableName, $db = 'master')
+    /**
+     * 更新 表属性 TableProperty
+     *
+     * @param string $tableName 表名
+     * @param string $dbName 库名
+     */
+    public function updateTableProperty($tableName, $dbName = 'master')
     {
-        $db = Be::getDb($db);
+        $db = Be::getDb($dbName);
 
         $fields = $db->getTableFields($tableName);
         $primaryKey = $db->getTablePrimaryKey($tableName);
 
         foreach ($fields as &$field) {
-            $field = (array)$field;
             if (strpos($field['extra'], 'auto_increment')) {
                 $field['autoIncrement'] = 1;
             } else {
@@ -51,75 +61,71 @@ class Db extends \Be\System\Service
         unset($field);
 
         $code = '<?php' . "\n";
-        $code .= 'namespace Be\\Cache\\System\\TableProperty\\' . $db . ';' . "\n";
+        $code .= 'namespace Be\\Cache\\System\\TableProperty\\' . $dbName . ';' . "\n";
         $code .= "\n";
         $code .= 'class ' . $tableName . ' extends \\Be\\System\\Db\\TableProperty' . "\n";
         $code .= '{' . "\n";
-        $code .= '    protected $_dbName = \'' . $db . '\'; // 数据库名' . "\n";
+        $code .= '    protected $_dbName = \'' . $dbName . '\'; // 数据库名' . "\n";
         $code .= '    protected $_tableName = \'' . $tableName . '\'; // 表名' . "\n";
         $code .= '    protected $_primaryKey = ' . var_export($primaryKey, true) . '; // 主键' . "\n";
         $code .= '    protected $_fields = ' . var_export($fields, true) . '; // 字段列表' . "\n";
         $code .= '}' . "\n";
         $code .= "\n";
 
-        $path = Be::getRuntime()->getCachePath() . '/System/TableProperty/' . $db . '/' . $tableName . '.php';
+        $path = Be::getRuntime()->getCachePath() . '/System/TableProperty/' . $dbName . '/' . $tableName . '.php';
         $dir = dirname($path);
         if (!is_dir($dir)) mkdir($dir, 0755, true);
 
         file_put_contents($path, $code, LOCK_EX);
         chmod($path, 0755);
-
-        return true;
     }
 
-
     /**
-     * 更新表
+     * 更新表 Table
      *
-     * @param string $tableName 要表新的表名
-     * @throws |Exception
+     * @param string $tableName 表名
+     * @param string $dbName 库名
      */
-    public function updateTable($tableName, $db = 'master')
+    public function updateTable($tableName, $dbName = 'master')
     {
-        $tableProperty = Be::getTableProperty($tableName, $db);
+        $tableProperty = Be::getTableProperty($tableName, $dbName);
 
         $code = '<?php' . "\n";
-        $code .= 'namespace Be\\Cache\\System\\Table\\' . $db . ';' . "\n";
+        $code .= 'namespace Be\\Cache\\System\\Table\\' . $dbName . ';' . "\n";
         $code .= "\n";
         $code .= 'class ' . $tableName . ' extends \\Be\\System\\Db\\Table' . "\n";
         $code .= '{' . "\n";
-        $code .= '    protected $_dbName = \'' . $db . '\'; // 数据库名' . "\n";
+        $code .= '    protected $_dbName = \'' . $dbName . '\'; // 数据库名' . "\n";
         $code .= '    protected $_tableName = \'' . $tableName . '\'; // 表名' . "\n";
         $code .= '    protected $_primaryKey = ' . var_export($tableProperty->getPrimaryKey(), true) . '; // 主键' . "\n";
         $code .= '    protected $_fields = [\'' . implode('\',\'', array_column($tableProperty->getFields(), 'name')) . '\']; // 字段列表' . "\n";
         $code .= '}' . "\n";
         $code .= "\n";
 
-        $path = Be::getRuntime()->getCachePath() . '/System/Table/' . $db . '/' . $tableName . '.php';
+        $path = Be::getRuntime()->getCachePath() . '/System/Table/' . $dbName . '/' . $tableName . '.php';
         $dir = dirname($path);
         if (!is_dir($dir)) mkdir($dir, 0755, true);
         file_put_contents($path, $code, LOCK_EX);
         chmod($path, 0755);
     }
 
-
     /**
-     * 更新 数据库行记灵对象
+     * 更新 行记灵对象 Tuple
      *
-     * @param string $name 数据库行记灵对象名称
-     * @throws |Exception
+     * @param string $tableName 表名
+     * @param string $dbName 库名
      */
-    public function updateTuple($tableName, $db = 'master')
+    public function updateTuple($tableName, $dbName = 'master')
     {
-        $tableProperty = Be::getTableProperty($tableName, $db);
+        $tableProperty = Be::getTableProperty($tableName, $dbName);
         $fields = $tableProperty->getFields();
 
         $code = '<?php' . "\n";
-        $code .= 'namespace Be\\Cache\\System\\Tuple\\' . $db . ';' . "\n";
+        $code .= 'namespace Be\\Cache\\System\\Tuple\\' . $dbName . ';' . "\n";
         $code .= "\n";
         $code .= 'class ' . $tableName . ' extends \\Be\\System\\Db\\Tuple' . "\n";
         $code .= '{' . "\n";
-        $code .= '    protected $_dbName = \'' . $db . '\'; // 数据库名' . "\n";
+        $code .= '    protected $_dbName = \'' . $dbName . '\'; // 数据库名' . "\n";
         $code .= '    protected $_tableName = \'' . $tableName . '\'; // 表名' . "\n";
         $code .= '    protected $_primaryKey = ' . var_export($tableProperty->getPrimaryKey(), true) . '; // 主键' . "\n";
 
@@ -137,7 +143,7 @@ class Db extends \Be\System\Service
         $code .= '}' . "\n";
         $code .= "\n";
 
-        $path = Be::getRuntime()->getCachePath() . '/System/Tuple/' . $db . '/' . $tableName . '.php';
+        $path = Be::getRuntime()->getCachePath() . '/System/Tuple/' . $dbName . '/' . $tableName . '.php';
         $dir = dirname($path);
         if (!is_dir($dir)) mkdir($dir, 0777, true);
 
