07e5701bc50364b23eae8e24fe9cb4b948752eb4
刘一二 <8981024@qq.com>
2020-08-31 16:01:40 +0800

    移除废弃文件

--- src/App/System/Template/AdminUser/js/admin_groups.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$("#admin_ui_category_row_1").addClass("success");
-	$("#admin_ui_category_row_1 input").prop("disabled", true);
-	$("#admin_ui_category_row_1 .delete").fadeOut();
-	
-	$(".ui-row").each(function(){
-		if($(".user_count", $(this)).length>0) $(".delete", $(this)).fadeOut();
-	});
-});	
