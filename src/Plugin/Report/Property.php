123b694d7c766567a1f0858b0d22c753322e217b
刘一二 <8981024@qq.com>
2020-11-18 16:25:13 +0800

    plugin report 报表

---
+++ src/Plugin/Report/Property.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Be\Plugin\Report;
+
+
+class Property extends \Be\System\Plugin\Property
+{
+
+    public $label = '报表';
+
+    public function __construct() {
+        parent::__construct(__FILE__);
+    }
+
+}
+
